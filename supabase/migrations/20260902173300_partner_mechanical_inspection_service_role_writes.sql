4fe5895ef7836f81376632385de92d8fb91cbd45
Jon B <132495876+GET-Jon@users.noreply.github.com>
2026-09-02 13:59:38 -0400

    Grant partner inspection writes to service role

---
+++ supabase/migrations/20260902173300_partner_mechanical_inspection_service_role_writes.sql
@@ -0,0 +1,3 @@
+grant update on table public.mindful_inventory_inspections to service_role;
+grant insert, update on table public.mindful_inventory_findings to service_role;
+grant insert on table public.mindful_inventory_history to service_role;
