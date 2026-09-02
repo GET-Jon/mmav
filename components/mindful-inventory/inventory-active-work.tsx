430978096f9789b15433678ece51703e15b45b7f
Jon B <132495876+GET-Jon@users.noreply.github.com>
2026-09-02 10:47:33 -0400

    Use quote-aware Active Work flow

--- components/mindful-inventory/inventory-active-work.tsx
+++ components/mindful-inventory/inventory-active-work.tsx
@@ -1,1 +1,1 @@
-export { InventoryActiveWork } from "@/components/mindful-inventory/inventory-active-work-v5";
+export { InventoryActiveWork } from "@/components/mindful-inventory/inventory-active-work-v6";
