246217f0fea7b74932625b689561aabfcc9e1ab5
Jon B <132495876+GET-Jon@users.noreply.github.com>
2026-09-02 10:05:15 -0400

    Switch active work to parts-first setup

--- components/mindful-inventory/inventory-active-work.tsx
+++ components/mindful-inventory/inventory-active-work.tsx
@@ -1,1 +1,1 @@
-export { InventoryActiveWork } from "@/components/mindful-inventory/inventory-active-work-v4";
+export { InventoryActiveWork } from "@/components/mindful-inventory/inventory-active-work-v5";
